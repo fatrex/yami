5e417a30afa830ae82d028ea15355506bc892a82
Daniele Lenares <hi@dnlnrs.dev>
2021-12-27 11:29:39 +0100

    chore: re-enable developer tools

--- src/main/menu.cjs
+++ src/main/menu.cjs
@@ -1,18 +1,33 @@
-const { Menu } = require('electron')
-const openSettingsWindow = require('./windows/settings.cjs')
+const { Menu } = require("electron");
+const openSettingsWindow = require("./windows/settings.cjs");
 
 const nativeMenus = [
   {
-    label: 'File',
+    label: "File",
     submenu: [
       {
-        label: 'Settings',
+        label: "Settings",
         click() {
-          openSettingsWindow()
-        }
-      }
-    ]
-  }
-]
-const menu = Menu.buildFromTemplate(nativeMenus)
-Menu.setApplicationMenu(menu)
+          openSettingsWindow();
+        },
+      },
+    ],
+  },
+  {
+    label: "View",
+    submenu: [
+      {
+        label: "Toggle Developer Tools",
+        accelerator: (function () {
+          if (process.platform == "darwin") return "Alt+Command+I";
+          else return "Ctrl+Shift+I";
+        })(),
+        click: function (_, focusedWindow) {
+          if (focusedWindow) focusedWindow.toggleDevTools();
+        },
+      },
+    ],
+  },
+];
+const menu = Menu.buildFromTemplate(nativeMenus);
+Menu.setApplicationMenu(menu);
