14c7831017b6454b8b0c9ca3a60d7c1a575f86de
Daniele Lenares <hi@dnlnrs.dev>
2021-12-27 12:00:18 +0100

    feat: add account logic in settings

--- src/renderer/routes/settings/accounts.svelte
+++ src/renderer/routes/settings/accounts.svelte
@@ -1,1 +1,13 @@
-Accounts Content
+<script>
+  import { accounts } from '../../stores'
+
+  import AccountChoice from '../../components/Account/AccountChoice.svelte';
+  import AccountNew from '../../components/Account/AccountNew.svelte';
+</script>
+
+<!-- Dropdown toggle button -->
+{#if $accounts.length > 0}
+  <AccountChoice />
+{:else}
+  <AccountNew />
+{/if}
